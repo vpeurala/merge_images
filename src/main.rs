a03a8d4e18a617945deeb64ff36a902d53701811
Ville Peurala <ville.peurala@op.fi>
2023-10-27 16:54:24 +0300

    Add --segment-length option

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
 use std::fs;
-use std::path::Path;
 use std::ffi::OsStr;
+use clap::{App, Arg};
 use indicatif::ProgressBar;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() < 3 {
-        println!("Usage: {} <output_dir> <input_dir_1> <input_dir_2> ...", args[0]);
-        std::process::exit(1);
-    }
+    let matches = App::new("Image Interleaver")
+        .about("Interleaves images from multiple directories into a single directory.")
+        .arg(
+            Arg::with_name("OUTPUT_DIR")
+                .help("Sets the output directory")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("INPUT_DIRS")
+                .help("Sets the input directories")
+                .required(true)
+                .multiple(true)
+                .index(2),
+        )
+        .arg(
+            Arg::with_name("SEGMENT_LENGTH")
+                .help("Sets the segment length")
+                .short("s")
+                .long("segment-length")
+                .takes_value(true)
+                .default_value("1"),
+        )
+        .get_matches();
 
-    let output_dir = &args[1];
-    let input_dirs: Vec<&String> = args[2..].iter().collect();
+    let output_dir = matches.value_of("OUTPUT_DIR").unwrap();
+    let input_dirs: Vec<&str> = matches.values_of("INPUT_DIRS").unwrap().collect();
+    let segment_length: usize = matches
+        .value_of("SEGMENT_LENGTH")
+        .unwrap()
+        .parse()
+        .expect("SEGMENT_LENGTH must be a positive integer");
 
     let mut files = Vec::new();
     let mut total_files = 0;
@@ -35,12 +59,14 @@ fn main() {
     loop {
         let mut did_something = false;
         for dir_files in files.iter_mut() {
-            if let Some(file) = dir_files.pop() {
-                let new_name = format!("{}/{:06}.png", output_dir, count);
-                fs::copy(&file, &new_name).unwrap_or_else(|_| panic!("Failed to copy file: {} to {}", file, new_name));
-                count += 1;
-                progress_bar.inc(1);
-                did_something = true;
+            for _ in 0..segment_length {
+                if let Some(file) = dir_files.pop() {
+                    let new_name = format!("{}/{:06}.png", output_dir, count);
+                    fs::copy(&file, &new_name).unwrap_or_else(|_| panic!("Failed to copy file: {} to {}", file, new_name));
+                    count += 1;
+                    progress_bar.inc(1);
+                    did_something = true;
+                }
             }
         }
         if !did_something {
